175fda9e7f9493fa8a67a8274aafdf099f3bf3c8
KMGl <113476451+KMOpenGL@users.noreply.github.com>
2022-11-03 15:29:48 -0700

    Menu Stuff

--- StarcraftChess/StarcraftChess.cpp
+++ StarcraftChess/StarcraftChess.cpp
@@ -35,6 +35,17 @@ public:
         }
         cachedModels.clear();
     }
+
+    Texture2D GetTexture(std::string name)
+    {
+        return LoadTexture((assetPath + "/textures/" + name + ".png").c_str());
+    }
+
+    Font GetFont(std::string name)
+    {
+        return LoadFont((assetPath + "/fonts/" + name + ".ttf").c_str());
+    }
+
     Shader GetShader(std::string name)
     {
         return LoadShader((assetPath + "/shaders/" + name + ".vs").c_str(),
@@ -396,18 +407,16 @@ int main()
 
     Model select = resourceInstance.GetModel("selected");
 
+    Font menuFont = resourceInstance.GetFont("Arial Bold");
+    Texture2D menuBG = resourceInstance.GetTexture("menuBG");
+
     raylib::Camera3D c = raylib::Camera3D({ 25,60,-30}, {-40,-18,-30}, {0,1,0}, 45, CAMERA_PERSPECTIVE);
 
     // Animation value
 
     float turnLerp = 0;
 
-    Piece p = Piece(5, 2, PieceType::Pawn, WHITE);
-    Piece p2 = Piece(4, 7, PieceType::Pawn, BLACK);
-    Piece p3 = Piece(3, 1, PieceType::Knight, WHITE);
-    Piece p4 = Piece(2, 1, PieceType::Bishop, WHITE);
-    Piece p5 = Piece(4, 1, PieceType::Queen, WHITE);
-    std::vector<Piece> pieces = {p, p2, p3, p4, p5};
+    std::vector<Piece> pieces = {};
 
     std::vector<Vector2> highlights = {};
 
@@ -418,210 +427,294 @@ int main()
 
     bool ai = false; // You can control both sets of pieces OR an AI controls black and you can control white
 
+    bool menu = true; // Whether or not if we should show the menu
+
     Piece* selected = NULL;
+
+    // Menu variables
+
+    int menuSelected = 0;
+
+    float menuLerp = 0;
+    float menuTime = 0;
+
+    Color clearColor = { 67,67,67,255 };
 
     while (!w.ShouldClose())
     {
-        bool currentTurn = !turn; // white or blacks turn
-
-        #pragma region End Turn Animation
-        if (turn && c.position.x > -100)
-        {
-            c.position.x = Lerp(25, -100, turnLerp);
-            if (turnLerp < 0.5)
-            {
-                c.position.z = Lerp(-30, 30, turnLerp / 0.5);
-                c.position.y = Lerp(60, 90, (turnLerp / 0.5));
-            }
-            else
-            {
-                c.position.y = Lerp(90, 60, (turnLerp - 0.5) / 0.5);
-                c.position.z = Lerp(30, -30, (turnLerp - 0.5) / 0.5);
-            }
-        }
-        else if (!turn && c.position.x < 25)
-        {
-            c.position.x = Lerp(-100, 25, turnLerp);
-            if (turnLerp < 0.5)
-            {
-                c.position.z = Lerp(-30, -60, turnLerp / 0.5);
-                c.position.y = Lerp(60, 90, (turnLerp / 0.5));
-            }
-            else
-            {
-                c.position.y = Lerp(90, 60, (turnLerp - 0.5) / 0.5);
-                c.position.z = Lerp(-60, -30, (turnLerp - 0.5) / 0.5);
-            }
-        }
-
-        if (turnLerp < 1)
-            turnLerp += GetFrameTime() * 2;
-
-        #pragma endregion End Turn Animation
-
-        #pragma region Gameplay
-
-        bool mDown = IsMouseButtonDown(0);
-        bool mReleased = IsMouseButtonReleased(0);
-
-        if (IsKeyPressed(KEY_SPACE))
-        {
-            std::cout << "end turn" << std::endl;
-            turn = !turn;
-            turnLerp = 0;
-            for (Piece& p : pieces)
-                p.hasMoved = false;
-        }
-
-        #pragma endregion Gameplay
-        
-        #pragma region Draw
-
         UpdateCamera(&c);
+        if (!menu)
+        {
+#pragma region Game
+            bool currentTurn = !turn; // white or blacks turn
+
+#pragma region End Turn Animation
+            if (turn && c.position.x > -100)
+            {
+                c.position.x = Lerp(25, -100, turnLerp);
+                if (turnLerp < 0.5)
+                {
+                    c.position.z = Lerp(-30, 30, turnLerp / 0.5);
+                    c.position.y = Lerp(60, 90, (turnLerp / 0.5));
+                }
+                else
+                {
+                    c.position.y = Lerp(90, 60, (turnLerp - 0.5) / 0.5);
+                    c.position.z = Lerp(30, -30, (turnLerp - 0.5) / 0.5);
+                }
+            }
+            else if (!turn && c.position.x < 25)
+            {
+                c.position.x = Lerp(-100, 25, turnLerp);
+                if (turnLerp < 0.5)
+                {
+                    c.position.z = Lerp(-30, -60, turnLerp / 0.5);
+                    c.position.y = Lerp(60, 90, (turnLerp / 0.5));
+                }
+                else
+                {
+                    c.position.y = Lerp(90, 60, (turnLerp - 0.5) / 0.5);
+                    c.position.z = Lerp(-60, -30, (turnLerp - 0.5) / 0.5);
+                }
+            }
+
+            if (turnLerp < 1)
+                turnLerp += GetFrameTime() * 2;
+
+#pragma endregion End Turn Animation
+
+#pragma region Gameplay
+
+            bool mDown = IsMouseButtonDown(0);
+            bool mReleased = IsMouseButtonReleased(0);
+
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                std::cout << "end turn" << std::endl;
+                turn = !turn;
+                turnLerp = 0;
+                for (Piece& p : pieces)
+                    p.hasMoved = false;
+            }
+
+#pragma endregion Gameplay
+
+#pragma region Draw
+
+            BeginDrawing();
+
+            w.ClearBackground(clearColor);
+
+            Vector2 mousePos = GetMousePosition();
+
+            DrawFPS(0, 0);
+
+            BeginMode3D(c);
+
+            DrawModelEx(board, { 0,0,0 }, { 1.0f, 0.0f, 0.0f }, 90.0f, { 1,1,1 }, WHITE);
+
+            // Selection box
+
+
+            for (int x = 1; x < 9; x++)
+            {
+                for (int y = 1; y < 9; y++)
+                {
+                    if (ChessHelper::Tile_IsHovered(x, y, c, turn))
+                    {
+                        Vector3 pos = ChessHelper::GridPos(x, y);
+                        // center
+                        pos.x += 8;
+                        pos.z -= 8;
+                        DrawModelEx(select, pos, { 1.0f,0.0f,0.0f }, -90, { 1,1,1 }, WHITE);
+                    }
+                }
+            }
+
+            if (selected)
+            {
+                Vector3 pos = ChessHelper::GridPos(selected->gX, selected->gY);
+                // center
+                pos.x += 8;
+                pos.y += 0.1;
+                pos.z -= 8;
+                DrawModelEx(select, pos, { 1.0f,0.0f,0.0f }, -90, { 1,1,1 }, GREEN);
+            }
+
+            bool wipeHighlights = true;
+
+            std::map<int, std::map<int, bool>> boardBool = convertBoardIntoBools(pieces);
+
+            bool selectedPiece = false;
+
+
+            for (Piece& p : pieces)
+            {
+                p.Draw();
+
+                // Check if you own the piece
+                // Kinda jank
+
+                if (ai)
+                    if ((p.c.r == 255 && turn) || p.c.r == 0)
+                        continue;
+                // Check if you clicked on em
+
+                bool canMove = ((p.c.r == 255 && !turn) || (p.c.r == 0 && turn)) && !p.hasMoved;
+
+                if (ChessHelper::Tile_IsHovered(p.gX, p.gY, c, turn) && selected == NULL && canMove)
+                {
+                    wipeHighlights = false;
+                    // would be o^2 if this wasn't just one piece. Luckily it is only one piece
+                    highlights = p.GetMoves(boardBool);
+
+                    if (mReleased)
+                    {
+                        selected = &p;
+                        selectedPiece = true;
+                    }
+                }
+            }
+
+            for (Vector2& highlight : highlights)
+            {
+                Vector3 pos = ChessHelper::GridPos(highlight.x, highlight.y);
+                // center
+                pos.x += 8;
+                pos.z -= 8;
+                pos.y += 0.1;
+                Color cc = YELLOW;
+                cc.a = 125;
+
+                bool stop = false;
+
+                int pId = 0;
+                int takeId = -1;
+                for (Piece& p : pieces)
+                {
+                    if (p.gX == highlight.x && p.gY == highlight.y)
+                    {
+                        if (currentTurn && p.c.r == 255)
+                        {
+                            stop = true;
+                            break;
+                        }
+                        else if (!currentTurn && p.c.r == 0)
+                        {
+                            stop = true;
+                            break;
+                        }
+                        else
+                            takeId = pId;
+                    }
+                    pId++;
+                }
+
+                if (stop)
+                    continue;
+
+                DrawModelEx(select, pos, { 1.0f,0.0f,0.0f }, -90, { 1,1,1 }, cc);
+
+                // detect if you click here
+
+                if (ChessHelper::Tile_IsHovered(highlight.x, highlight.y, c, turn))
+                {
+                    if (mDown && selected)
+                    {
+                        selected->Move(highlight.x, highlight.y);
+                        if (takeId != -1)
+                            pieces.erase(pieces.begin() + takeId);
+
+                        selected = NULL;
+                    }
+                }
+            }
+
+
+            if (mDown && !selectedPiece)
+                selected = NULL;
+
+            if (wipeHighlights && selected == NULL)
+                highlights.clear();
+
+            EndMode3D();
+            DrawText(("Mouse Pos: " + std::to_string((int)mousePos.x) + "," + std::to_string((int)mousePos.y)).c_str(), 0, 24, 18, WHITE);
+            DrawText(("Turn Lerp: " + std::to_string(turnLerp)).c_str(), 0, 48, 18, WHITE);
+            DrawText(("Highlights: " + std::to_string(highlights.size())).c_str(), 0, 72, 18, WHITE);
+
+            EndDrawing();
+#pragma endregion Draw
+
+#pragma endregion Game
+        }
+        else
+        {
+#pragma region Menu
+
+        if (IsKeyPressed(KEY_DOWN))
+            menuSelected++;
+        if (IsKeyPressed(KEY_UP))
+            menuSelected--;
+
+        if (menuSelected > 1)
+            menuSelected = 0;
+        if (menuSelected < 0)
+            menuSelected = 1;
+
+        // camera movement
+
+        menuTime += GetFrameTime() * 0.08;
+
+        if (menuTime > 1)
+            menuTime = 0;
+
+        menuLerp = menuTime / 1;
+
+        if (menuLerp < 0.25)
+        {
+            c.position.x = Lerp(25, -100, menuLerp / 0.25);
+            c.position.z = Lerp(-30, 0, menuLerp / 0.25);
+        }
+        else if (menuLerp < 0.5)
+        {
+            c.position.x = Lerp(-100, -80, (menuLerp - 0.25) / 0.25);
+            c.position.z = Lerp(0, -60, (menuLerp - 0.25) / 0.25);
+        }
+        else if (menuLerp > 0.5)
+        {
+            c.position.x = Lerp(-80, 25, (menuLerp - 0.5) / 0.5);
+            c.position.z = Lerp(-60, -30, (menuLerp - 0.5) / 0.5);
+        }
 
         BeginDrawing();
 
-        w.ClearBackground(DARKGRAY);
-
-        Vector2 mousePos = GetMousePosition();
-
-        DrawFPS(0, 0);
+        w.ClearBackground(clearColor);
 
         BeginMode3D(c);
 
         DrawModelEx(board, { 0,0,0 }, { 1.0f, 0.0f, 0.0f }, 90.0f, { 1,1,1 }, WHITE);
-        
-        // Selection box
-
-
-        for (int x = 1; x < 9; x++)
-        {
-            for (int y = 1; y < 9; y++)
-            {
-                if (ChessHelper::Tile_IsHovered(x, y, c, turn))
-                {
-                    Vector3 pos = ChessHelper::GridPos(x, y);
-                    // center
-                    pos.x += 8;
-                    pos.z -= 8;
-                    DrawModelEx(select, pos, { 1.0f,0.0f,0.0f }, -90, { 1,1,1 }, WHITE);
-                }
-            }
-        }
-
-        if (selected)
-        {
-            Vector3 pos = ChessHelper::GridPos(selected->gX, selected->gY);
-            // center
-            pos.x += 8;
-            pos.y += 0.1;
-            pos.z -= 8;
-            DrawModelEx(select, pos, { 1.0f,0.0f,0.0f }, -90, { 1,1,1 }, GREEN);
-        }
-
-        bool wipeHighlights = true;
-
-        std::map<int, std::map<int, bool>> boardBool = convertBoardIntoBools(pieces);
-
-        bool selectedPiece = false;
-
-
-        for (Piece& p : pieces)
-        {
-            p.Draw();
-
-            // Check if you own the piece
-            // Kinda jank
-
-            if (ai)
-                if ((p.c.r == 255 && turn) || p.c.r == 0)
-                    continue;
-            // Check if you clicked on em
-
-            bool canMove = ((p.c.r == 255 && !turn) || (p.c.r == 0 && turn)) && !p.hasMoved;
-
-            if (ChessHelper::Tile_IsHovered(p.gX, p.gY, c, turn) && selected == NULL && canMove)
-            {
-                wipeHighlights = false;
-                // would be o^2 if this wasn't just one piece. Luckily it is only one piece
-                highlights = p.GetMoves(boardBool);
-
-                if (mReleased)
-                {
-                    selected = &p;
-                    selectedPiece = true;
-                }
-            }
-        }
-
-        for (Vector2& highlight : highlights)
-        {
-            Vector3 pos = ChessHelper::GridPos(highlight.x, highlight.y);
-            // center
-            pos.x += 8;
-            pos.z -= 8;
-            pos.y += 0.1;
-            Color cc = YELLOW;
-            cc.a = 125;
-
-            bool stop = false;
-
-            int pId = 0;
-            int takeId = -1;
-            for (Piece& p : pieces)
-            {
-                if (p.gX == highlight.x && p.gY == highlight.y)
-                {
-                    if (currentTurn && p.c.r == 255)
-                    {
-                        stop = true;
-                        break;
-                    }
-                    else if (!currentTurn && p.c.r == 0)
-                    {
-                        stop = true;
-                        break;
-                    }
-                    else
-                        takeId = pId;
-                }
-                pId++;
-            }
-
-            if (stop)
-                continue;
-
-            DrawModelEx(select, pos, { 1.0f,0.0f,0.0f }, -90, { 1,1,1 }, cc);
-
-            // detect if you click here
-
-            if (ChessHelper::Tile_IsHovered(highlight.x, highlight.y, c, turn))
-            {
-                if (mDown && selected)
-                {
-                    selected->Move(highlight.x, highlight.y);
-                    if (takeId != -1)
-                        pieces.erase(pieces.begin() + takeId);
-
-                    selected = NULL;
-                }
-            }
-        }
-
-
-        if (mDown && !selectedPiece)
-            selected = NULL;
-
-        if (wipeHighlights && selected == NULL)
-            highlights.clear();
 
         EndMode3D();
-        DrawText(("Mouse Pos: " + std::to_string((int)mousePos.x) + "," + std::to_string((int)mousePos.y)).c_str(), 0, 24, 18, WHITE);
-        DrawText(("Turn Lerp: " + std::to_string(turnLerp)).c_str(), 0, 48, 18, WHITE);
-        DrawText(("Highlights: " + std::to_string(highlights.size())).c_str(), 0, 72, 18, WHITE);
+
+        DrawTexture(menuBG, -360, 0, WHITE);
+
+        DrawTextEx(menuFont, "Starcraft Chess", { 25,25 }, 64, 1, WHITE);
+        switch (menuSelected)
+        {
+        case 0:
+            DrawTextEx(menuFont, "PLAY", { 25,104 }, 42, 1, WHITE);
+            DrawTextEx(menuFont, "QUIT", { 25,158 }, 42, 1, DARKGRAY);
+            break;
+        case 1:
+            DrawTextEx(menuFont, "PLAY", { 25,104 }, 42, 1, DARKGRAY);
+            DrawTextEx(menuFont, "QUIT", { 25,158 }, 42, 1, WHITE);
+            break;
+        }
 
         EndDrawing();
-        #pragma endregion Draw
+
+#pragma endregion Menu
+        }
     }
 
 
